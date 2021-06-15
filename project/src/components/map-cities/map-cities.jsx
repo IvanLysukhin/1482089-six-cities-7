b9cdf493fd21333517c211ce4626bc5dbc256cee
Ivan Lysukhin <ilysukhin@mail.ru>
2021-06-15 19:53:39 +0300

    added custom map hook

--- project/src/components/map-cities/map-cities.jsx
+++ project/src/components/map-cities/map-cities.jsx
@@ -1,46 +1,30 @@
-import React, {useEffect, useRef} from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import {PropTypes} from 'prop-types';
 import offerProp from '../place-card/place-card.prop';
+import useMap from '../../hooks/useMap';
 import leaflet from 'leaflet';
-import 'leaflet/dist/leaflet.css';
 
-const AMSTERDAM = [52.38333, 4.9];
-const ZOOM = 12;
-
-function MapCities ({offers}) {
+function MapCities({offers}) {
   const mapRef = useRef(null);
+  const mapCities = useMap(mapRef, offers[0].city);
+
+  const icon = leaflet.icon({
+    iconUrl: 'img/pin.svg',
+    iconSize: [30, 40],
+  });
 
   useEffect(() => {
-    mapRef.current = leaflet.map('map', {
-      center: AMSTERDAM,
-      zoom: ZOOM,
-      zoomControl: false,
-      marker: true,
-    });
-
-    const icon = leaflet.icon({
-      iconUrl: 'img/pin.svg',
-      iconSize: [30, 40],
-    });
-
-    leaflet
-      .tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png', {
-        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
-      })
-      .addTo(mapRef.current);
-
-    offers.forEach(({location}) => {
-      leaflet
-        .marker(
-          [location.latitude, location.longitude],
-          {icon},
-        )
-        .addTo(mapRef.current);
-    });
-
-    return ()=> mapRef.current.remove();
-
-  }, []);
+    if (mapCities) {
+      offers.forEach(({location}) => {
+        leaflet
+          .marker(
+            [location.latitude, location.longitude],
+            {icon},
+          )
+          .addTo(mapCities);
+      });
+    }
+  }, [mapCities, offers]);
 
   return (
     <section
--- project/src/mocks/offers.js
+++ project/src/mocks/offers.js
@@ -51,9 +51,9 @@ export const STATIC_OFFERS = [
     bedRooms: 2,
     city: {
       location: {
-        latitude: 52.370213,
-        longitude: 4.895132,
-        zoom: 10,
+        latitude: 52.38333,
+        longitude: 4.9,
+        zoom: 12,
       },
       name: 'Tokyo',
     },
