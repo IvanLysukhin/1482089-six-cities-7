05103c6af0afb87be8622960262e04dc97d7a879
Ivan Lysukhin <ilysukhin@mail.ru>
2021-07-06 18:32:58 +0300

    test for reviews list

--- project/src/components/review/review.test.js
+++ project/src/components/review/review.test.js
@@ -2,11 +2,7 @@ import React from 'react';
 import {render, screen} from '@testing-library/react';
 import {Router} from 'react-router-dom';
 import {createMemoryHistory} from 'history';
-import configureStore from 'redux-mock-store';
-import {Provider} from 'react-redux';
 import Review from './review';
-
-const mockStore = configureStore({});
 
 describe('Component: Review', () => {
   it('should render "Review" correctly', () => {
