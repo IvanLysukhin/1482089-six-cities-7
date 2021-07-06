0fe2be2ffc32e8bc25825e8dbbafb69582f6d9be
Ivan Lysukhin <ilysukhin@mail.ru>
2021-07-06 18:23:22 +0300

    test for review component

---
+++ project/src/components/review/review.test.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import {render, screen} from '@testing-library/react';
+import {Router} from 'react-router-dom';
+import {createMemoryHistory} from 'history';
+import configureStore from 'redux-mock-store';
+import {Provider} from 'react-redux';
+import Review from './review';
+
+const mockStore = configureStore({});
+
+describe('Component: Review', () => {
+  it('should render "Review" correctly', () => {
+    const history = createMemoryHistory();
+    const fakeReview = {
+      id: 1,
+      user: {
+        id: 14,
+        isPro: true,
+        name: 'Corey',
+        avatarUrl: 'https://7.react.pages.academy/static/avatar/5.jpg',
+      },
+      rating: 4,
+      comment: 'I stayed here for one night and it was an unpleasant experience.',
+      date: '2021-07-01T13:04:25.833Z',
+    };
+    render(
+        <Router history={history}>
+          <Review review={fakeReview}/>
+        </Router>
+    );
+
+
+    expect(screen.getByText('Corey')).toBeInTheDocument();
+    expect(screen.getByText('I stayed here for one night and it was an unpleasant experience.')).toBeInTheDocument();
+    expect(screen.getByText('July 1')).toBeInTheDocument();
+    expect(screen.getByText('4')).toBeInTheDocument();
+  });
+});
