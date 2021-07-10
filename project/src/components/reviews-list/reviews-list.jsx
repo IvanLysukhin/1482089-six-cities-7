ca0ba9d832248f809d18f81e973e367ab8ff609e
Ivan Lysukhin <ilysukhin@mail.ru>
2021-07-10 16:57:15 +0300

    sort reviews

--- project/src/components/reviews-list/reviews-list.jsx
+++ project/src/components/reviews-list/reviews-list.jsx
@@ -5,19 +5,17 @@ import reviewProp from '../review/review.prop';
 
 function ReviewsList({reviews}) {
   const maxReviewsNumber = 10;
-  // console.log(reviews.length);
-  // if (reviews.length) {
-  //   reviews = reviews.sort((a, b) => {
-  //     const dateA = new Date(a.date);
-  //     const dateB = new Date(b.date);
-  //     console.log(dateA, dateB)
-  //   })
-  // }
 
+  const sortedReviews = [...reviews].sort((a, b) => {
+    const dateA = new Date(a.date);
+    const dateB = new Date(b.date);
+    return dateB.valueOf() - dateA.valueOf();
+  });
+  
   return (
     <ul className="reviews__list">
       {
-        reviews
+        sortedReviews
           .slice(0, maxReviewsNumber)
           .map((review) => <Review review={review} key={review.id}/>)
       }
